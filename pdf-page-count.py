ce176298c9bd66f53bf66a1bf23a33d94859256a
Geno Sanchez <39529787+genoes@users.noreply.github.com>
2021-04-27 11:16:06 -0700

    Create pdf-page-count.py

---
+++ pdf-page-count.py
@@ -0,0 +1,18 @@
+import PyPDF2 , os
+
+pdf_directory = input('\n'"Enter absolute path to PDFs: ").strip()
+output_csv = "pdf-page-count.csv"
+lst = os.listdir(pdf_directory)
+lst.sort()
+
+for filename in lst:
+    if filename.endswith('.pdf'):
+        file = (os.path.join(pdf_directory, filename))
+        pdfFileObj = open(file,'rb')
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        num_pages = pdfReader.numPages
+
+        with open  (output_csv, "a") as f:
+            print(filename, ",", num_pages, "pages", file=f)
+
+print('\n'"Done!")
